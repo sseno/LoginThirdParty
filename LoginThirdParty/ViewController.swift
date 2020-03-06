0dc4123a6ae25ff34a00722d9f835e1e63570866
Rohmats <rohmatsuseno@gmail.com>
2020-03-06 19:40:38 +0700

    Initial Commit

---
+++ LoginThirdParty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginThirdParty
+//
+//  Created by Rohmat Suseno on 06/03/20.
+//  Copyright © 2020 Rohmts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
